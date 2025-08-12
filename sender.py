68d956b57555bfeefcba3fa140cdaf8924b5d56c
nick-p-34 <117747730+nick-p-34@users.noreply.github.com>
2025-08-12 17:38:50 +0100

    Increase response timeout limit to 10 seconds

--- sender.py
+++ sender.py
@@ -6,7 +6,7 @@ from constants import SEND_TO_SERVER, SERVER_URL, OUTPUT_FILE
 def send_event(event: dict):
     if SEND_TO_SERVER:
         try:
-            response = requests.post(SERVER_URL, json=event, timeout=2.0)
+            response = requests.post(SERVER_URL, json=event, timeout=10.0)
 
             if response.status_code >= 400:
                 print(f"Error sending event: {response.status_code} {response.text}")
